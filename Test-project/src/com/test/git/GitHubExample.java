3a3aa4175b11c81c9a8518af2a0e63a86fbcd4c0
kanakesh-rccl <knarimi@rccl.com>
2020-05-24 14:43:28 +0530

    First commit to test the github project

---
+++ Test-project/src/com/test/git/GitHubExample.java
@@ -0,0 +1,11 @@
+package com.test.git;
+
+public class GitHubExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is github project execution example");
+
+	}
+
+}
